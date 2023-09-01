5b3a801565463b106f4b71a413e4e4bc04585263
ishdeena <shariq.forever@gmail.com>
2023-08-31 23:53:02 -0400

    aap.py changed

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ def index():
     return render_template('index.html', weather=weather, forecast=forecast, alerts=alerts)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=os.environ.get('PORT'))
+
